87c852bda4dc92f1c158ccb710a58cd25532dff7
tsinghoo <tsinghoo@hotmail.com>
2023-05-12 18:54:24 +0800

    update script

--- run.sh
+++ run.sh
@@ -3,8 +3,11 @@
 set -x
 #download
 m3u8='https://video.qianliao.net/43842e87vodtranscq1500005495/982f8770243791581774501290/video_950555_2.m3u8?sign=f25695916ae06c7ea0ee02c47a9aa78f&t=645e46c9'
+m3u8="https://video.qianliao.net/43842e87vodtranscq1500005495/2beeac4b243791582056401346/video_950555_2.m3u8?sign=cc33740225b6e7567e7747b68c2a26c9&t=645e6cc1"
 
 merged=temp.1.ts
 ff="-vf scale=-1:240 -acodec copy -preset veryslow -crf 28"
 
-./xiaoetong -u $m3u8 -ff "$ff" -n $merged -threads 2 -t 11
+./xiaoetong -u $m3u8 -ff "$ff" -n $merged -threads 2 -t 0
+
+#ffmpeg -ss 00:47:40 -to 00:52:38  -i "yanzilong.ts" -s 640x360 -crf 28  -acodec copy  yanzilong07.mp4
