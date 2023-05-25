5a4fa5122b2ff0928de123f284596317783b60e2
tsinghoo <tsinghoo@hotmail.com>
2023-05-25 21:14:56 +0800

    temp

--- run.sh
+++ run.sh
@@ -2,6 +2,13 @@
 # go build
 set -x
 #download
+#hu
+m3u8='https://video.qianliao.net/43842e87vodtranscq1500005495/982f8770243791581774501290/video_950555_2.m3u8?sign=f25695916ae06c7ea0ee02c47a9aa78f&t=645e46c9'
+#Yuan
+m3u8="https://video.qianliao.net/43842e87vodtranscq1500005495/2beeac4b243791582056401346/video_950555_2.m3u8?sign=cc33740225b6e7567e7747b68c2a26c9&t=645e6cc1"
+#shen
+m3u8="https://video.qianliao.net/43842e87vodtranscq1500005495/c510d6753270835008954474999/video_950555_1.m3u8?sign=091e959f2af899ec90e1bf36bc5c2786&t=645f34af"
+#xiong
 m3u8="https://video.qianliao.net/43842e87vodtranscq1500005495/cd65d8203270835008906974310/video_950555_1.m3u8?sign=61d9c9cc6832f902720b856ae8ea2eee&t=645f3a4c"
 m3u8="https://mps-trans.yzcdn.cn/multi_trans_hls_hd/67j57BpSVwY-VWZDvLklgv96sG8bcS8_3Lymog_HD.m3u8?sign=257ace0911d3831f37a5db688b610034&t=6466d928"
 m3u8="https://video.qianliao.net/43842e87vodtranscq1500005495/db125433243791581751601387/video_950555_1.m3u8?sign=99dea2a2fad03d5e6900b1c9539d154c&t=6466440a"
